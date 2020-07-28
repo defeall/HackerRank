c9d3f7d65535efe7097952ba9eaeff2e6baaa4df
Niraj Kumar Lathar <62565529+defeall@users.noreply.github.com>
2020-07-28 23:50:18 +0530

    Add files via upload

---
+++ C++/Basic Data Types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    char c;
+    float d;
+    long b;
+    double e;
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%ld\n%c\n%f\n%lf",a,b,c,d,e);
+    return 0;
+}
